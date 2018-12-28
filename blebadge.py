59bd7c33564169c4f65e13074b9a2c1adc544719
Jakob Löw <jakob@m4gnus.de>
2018-12-28 22:22:52 +0100

    the marquee and blink bytes are actually bitfields, with one bit per message

--- blebadge.py
+++ blebadge.py
@@ -21,8 +21,8 @@ parser.add_argument("--speed", type=int, default=4, choices=range(1, 9),
 	help="Set animation speed")
 parser.add_argument("--mode", default="left", choices=modes,
 	help="Set animation mode")
-parser.add_argument("--flash", action="store_true",
-	help="Enable message flashing")
+parser.add_argument("--blink", action="store_true",
+	help="Enable message blinking")
 parser.add_argument("--marquee", action="store_true",
 	help="Enable moving circle around text")
 #parser.add_argument("--timestamp", action="store_true",
@@ -33,23 +33,29 @@ parser.add_argument("--text",
 	help="The text to send to the badge")
 parser.add_argument("--file", type=open,
 	help="Input file to read texts from. " +
-		"Format is <speed>,<mode>,<text ...>. " +
+		"Format is <speed 1-8>,<mode>,<blink 0|1>,<marquee 0|1>,<text ...>. " +
 		"One message per line, up to 8 messages are supported")
 
 args = parser.parse_args()
 
 messages = []
 if args.text:
-	messages.append((args.speed, args.mode, args.text))
+	messages.append((args.speed, args.mode, args.blink, args.marquee, args.text))
 elif args.file:
 	for line in args.file:
 		if line[-1] == "\n":
 			line = line[0:-1]
 
 		split = line.split(",")
+		if len(split) < 5:
+			print("Invalid line in input file")
+			exit(1)
+
 		speed = int(split[0])
 		mode = split[1]
-		text = ",".join(split[2:])
+		blink = int(split[2]) == 1
+		marquee = int(split[3]) == 1
+		text = ",".join(split[4:])
 
 		if speed < 1 or speed > 8:
 			print("Invalid speed " + split[0])
@@ -57,11 +63,8 @@ elif args.file:
 		if mode not in modes:
 			print("Invalid mode " + mode)
 			exit(1)
-		if len(text) <= 0:
-			print("Invalid text size")
-			exit(1)
 
-		messages.append((speed, mode, text))
+		messages.append((speed, mode, blink, marquee, text))
 else:
 	print("Either --text or --file must be given")
 	exit(1)
@@ -72,8 +75,8 @@ spacer = [0 for x in range(0, 16)]
 
 start = [
 	0x77, 0x61, 0x6e, 0x67, 0x00, 0x00, #message header
-	1 if args.flash else 0,
-	1 if args.marquee else 0
+	0, # blink flag, one bit per message
+	0, # marquee, one bit per message
 ]
 sizes = []
 values = []
@@ -85,10 +88,15 @@ for i in range(0, 8):
 		sizes.append(0)
 		continue
 	
-	speed, mode, text = messages[i]
+	speed, mode, blink, marquee, text = messages[i]
 	options = ((speed - 1) << 4) | modes.index(mode)
 	textLen = len(text)
-	
+
+	if blink:
+		start[6] = start[6] | (1 << i)
+	if marquee:
+		start[7] = start[7] | (1 << i)
+
 	start.append(options)
 
 	sizes.append((textLen & 0xffff) >> 8)
